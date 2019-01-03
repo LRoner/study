a46a3d4a82471d2ca648bf64e7ff343c7c4f9c9d
liubo <liubo_roner@163.com>
2019-01-03 17:57:28 +0800

    首页

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>学习git笔记</title>
 </head>
 <body>
 	<h1>这是我的第一个首页</h1>
+	<?php
+$arr=['西瓜','苹果','鸭梨'];
+foreach ($arr as $key => $value) {
+	echo "<li>我是第{$key}位<li>:我的值是{$value}!";
+}
+?>
 </body>
 </html>
